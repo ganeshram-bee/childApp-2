ed63234a6549d79f1f2468689b8360ea49ab8dee
ganeshram-bee <ganesh.ram@krazybee.com>
2020-02-05 15:42:28 +0530

    hfdksa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Link } from 'react-router-dom'
 function App() {
   return (
+  	<BrowserRouter basename={"/parent"}>
     <div className="App">
-        Hello from app 3
+        Hello from child app 2
+        <p>
+        	<button 
+            onClick={() => {
+              console.log("informing parent to change pathName from app2");
+              window.e.emit('goTo', {pathName: '/childApp3'});
+            }}
+          >
+            Go to childApp3
+          </button>
+        </p>
     </div>
+    </BrowserRouter>
   );
 }
 
